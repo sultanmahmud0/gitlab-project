ab8a58039c9ba0616cacbf7db7eef00105181007
sultan <sultan@cefalo.com>
2024-08-21 14:26:55 +0600

    Test message

---
+++ src/main/java/com/example/gitlabdemo/GitlabdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.gitlabdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabdemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("new gitlab demo");
+        SpringApplication.run(GitlabdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitlabdemo/GitlabdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitlabdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
